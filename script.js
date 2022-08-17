8748606a44319e9e88a0a0d0cb56374c08f60c55
evanboyd31 <evan.boyd@mail.mcgill.ca>
2022-08-16 20:15:09 -0700

    Add hover effect to color each cell in the grid of divs

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const gridContainer = document.querySelector(".grid-container");
-let squareGrids = [];
 
 for (i = 0; i < 256; i++) {
     let node = document.createElement("div");
     node.classList.add("cell");
-    squareGrids.push(node);
+    node.onmouseover = function (event) {
+        let target = event.target;
+        target.style.background = 'black';
+    };
     gridContainer.appendChild(node);
 }
-
